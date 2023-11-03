2cda85c1bb70cfa9aea262eb6995d1ba11fc486b
zaidasifmalik <zaidasif132@gmail.com>
2023-11-03 16:41:05 +0500

    gpt backend

--- index.js
+++ index.js
@@ -9,28 +9,60 @@ const mongoose = require("mongoose");
 const auth = require("./routes/auth");
 const rooms = require("./routes/rooms");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
+
 // app.use(bodyParser.json());
 app.use(cors()); // This allows all origin
 app.use(express.json());
 app.use("/api/auth", auth);
-app.use('/api/room',rooms);
-const io = new Server(server,{
-  cors:{
-    origin:"*"
-  }
+app.use("/api/room", rooms);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
+
 const PORT = process.env.PORT || 8080;
 
-
 io.on("connection", (socket) => {
-  console.log("user connected: ",socket.id);
-  socket.on('join-room',(room)=> {
+  socket.on("join-room", (room, user) => {
     socket.join(room);
-  })
+    socket.to(room).emit("user-joined", user);
+  });
+
+  socket.on("leave-room", (user, room) => { 
+    socket.to(room).emit("user-left", user);
+  });
+
+  socket.on("send-message", (message, room) => {
+    socket.to(room).emit("recieve-message", message);
+  });
 });
 
+//Stripe Integration
+const stripe = require("stripe")(
+  "sk_test_51NoQGmSDkACrq3oOCKvupOYTvJadCs6ErCYcArqHezd5zJcsJ3gRq97t6CVBLEbOi5TLSKpRomIDJ0muzRL9a5lq003WgZV7oc"
+);
+const calculateOrderAmount = (items) => {
+  return items;
+};
 
+app.post("/create-payment-intent", async (req, res) => {
+  const { totalAmount } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount * 100, //Current Indian Currency Rate
+    currency: "inr",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -43,5 +75,5 @@ mongoose
   });
 
 setInterval(() => {
-    DeleteExpiredRooms();
-}, 1000*60);
+  DeleteExpiredRooms();
+}, 1000 * 60);
